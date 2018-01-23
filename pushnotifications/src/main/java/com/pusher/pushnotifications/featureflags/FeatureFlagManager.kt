fee284c359b3f11e3fcfdcf9dac01f6527b82570
Jonathan Lloyd <jonathan@thisisjonathan.com>
2018-01-23 14:27:02 +0000

    Add feature flag to disable delivery tracking

---
+++ pushnotifications/src/main/java/com/pusher/pushnotifications/featureflags/FeatureFlagManager.kt
@@ -0,0 +1,20 @@
+package com.pusher.pushnotifications.featureflags
+
+/*
+ * Tells the SDK which features should be enabled. May be extended in the future to get this
+ * information from the server.
+ */
+enum class FeatureFlag {
+    DELIVERY_TRACKING
+}
+
+object FeatureFlagManager {
+
+    private val flags: Map<FeatureFlag, Boolean> = mapOf(
+        FeatureFlag.DELIVERY_TRACKING to false
+    )
+
+    fun isEnabled(flag: FeatureFlag): Boolean {
+        return flags[flag]!!
+    }
+}
--- pushnotifications/src/main/java/com/pusher/pushnotifications/reporting/FCMMessageReceiver.kt
+++ pushnotifications/src/main/java/com/pusher/pushnotifications/reporting/FCMMessageReceiver.kt
@@ -7,6 +7,8 @@ import android.util.Log
 import com.firebase.jobdispatcher.*
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
+import com.pusher.pushnotifications.featureflags.FeatureFlag
+import com.pusher.pushnotifications.featureflags.FeatureFlagManager
 import com.pusher.pushnotifications.logging.Logger
 import com.pusher.pushnotifications.reporting.api.ReportEvent
 import com.pusher.pushnotifications.reporting.api.ReportEventType
@@ -22,6 +24,11 @@ class FCMMessageReceiver : WakefulBroadcastReceiver() {
 
   override fun onReceive(context: Context?, intent: Intent?) {
     Log.i("FCMMessageReceiver", intent.toString())
+
+    if (!FeatureFlagManager.isEnabled(FeatureFlag.DELIVERY_TRACKING)) {
+      Log.i("FCMMessageReceiver", "Delivery tracking flag is disabled. Skipping.")
+      return
+    }
 
     intent?.getStringExtra("pusher")?.let { pusherDataJson ->
       try {
