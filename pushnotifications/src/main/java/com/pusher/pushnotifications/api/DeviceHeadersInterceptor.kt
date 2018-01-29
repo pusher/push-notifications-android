be88945a3a88aab9dbe4126c8a4af3e7fd9a68cc
Jonathan Lloyd <jonathan@thisisjonathan.com>
2018-01-29 11:54:26 +0000

    Format fix

--- pushnotifications/src/main/java/com/pusher/pushnotifications/api/DeviceHeadersInterceptor.kt
+++ pushnotifications/src/main/java/com/pusher/pushnotifications/api/DeviceHeadersInterceptor.kt
@@ -5,14 +5,14 @@ import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
-class DeviceHeadersInterceptor(): Interceptor {
-    private val sdkVersion = BuildConfig.VERSION_NAME
+class DeviceHeadersInterceptor : Interceptor {
+  private val sdkVersion = BuildConfig.VERSION_NAME
 
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request();
-        val newRequest = request.newBuilder()
-                .addHeader("x-pusher-library" ,"push-notifications-android $sdkVersion")
-                .build()
-        return chain.proceed(newRequest)
-    }
+  override fun intercept(chain: Interceptor.Chain): Response {
+    val request = chain.request();
+    val newRequest = request.newBuilder()
+        .addHeader("x-pusher-library", "push-notifications-android $sdkVersion")
+        .build()
+    return chain.proceed(newRequest)
+  }
 }
