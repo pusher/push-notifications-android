3cf083fff111bd84fb987fca725b52ed3bae8b5c
luismfonseca <miguelluisfonseca@gmail.com>
2018-12-07 12:01:46 +0000

    Add basic in-memory storage for more realistic tests

--- pushnotifications-integration-tests/src/androidTest/java/com/example/pushnotificationsintegrationtests/FakeErrol.kt
+++ pushnotifications-integration-tests/src/androidTest/java/com/example/pushnotificationsintegrationtests/FakeErrol.kt
@@ -1,27 +1,49 @@
 package com.example.pushnotificationsintegrationtests
 
 import fi.iki.elonen.NanoHTTPD
+import java.util.*
+
+data class FakeErrolDevice(
+  val id: String,
+  val interests: MutableSet<String>
+) {
+  companion object {
+    fun New(interests: MutableSet<String>): FakeErrolDevice {
+      return FakeErrolDevice(UUID.randomUUID().toString(), interests)
+    }
+  }
+}
+
+data class FakeErrolStorage(
+  val devices: MutableMap<String, FakeErrolDevice>
+)
 
 class FakeErrol(port: Int): NanoHTTPDRouter(port) {
+  val storage = FakeErrolStorage(mutableMapOf())
+
   init {
     start(NanoHTTPD.SOCKET_READ_TIMEOUT, false)
   }
 
   override fun setupRoutes() {
     post("/instances/{instanceId}/devices/fcm") { session, params ->
+      val device = FakeErrolDevice.New(mutableSetOf())
+      storage.devices[device.id] = device
+
       NanoHTTPD.newFixedLengthResponse(Response.Status.OK, mimeTypeJSON, """
         {
-          "id": "d-123",
+          "id": "${device.id}",
           "initialInterestSet": []
         }
       """.trimIndent())
     }
 
     get("/instances/{instanceId}/devices/fcm/{deviceId}") { session, params ->
-      if (params["deviceId"] == "d-123") {
+      val device = storage.devices[params["deviceId"]]
+      if (device != null) {
         NanoHTTPD.newFixedLengthResponse(Response.Status.OK, mimeTypeJSON, """
           {
-            "id": "d-123",
+            "id": "${device.id}",
             "metadata": {
               "sdkVersion": "",
               "androidVersion": ""
