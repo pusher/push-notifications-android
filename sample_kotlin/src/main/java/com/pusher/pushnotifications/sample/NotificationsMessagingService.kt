92ea98a6ccef272b48751bb6ab8d508bed77b273
Jonathan Lloyd <jonathan@thisisjonathan.com>
2019-02-28 11:26:20 +0000

    Forward onNewToken method to MessagingService

--- sample_kotlin/src/main/java/com/pusher/pushnotifications/sample/NotificationsMessagingService.kt
+++ sample_kotlin/src/main/java/com/pusher/pushnotifications/sample/NotificationsMessagingService.kt
@@ -5,6 +5,11 @@ import com.google.firebase.messaging.RemoteMessage
 import com.pusher.pushnotifications.fcm.MessagingService
 
 class NotificationsMessagingService : MessagingService() {
+  override fun onNewToken(token: String) {
+    Log.i("NotificationsService", "Token was updated 🔒")
+    // TODO: Do something useful here
+  }
+
   override fun onMessageReceived(remoteMessage: RemoteMessage) {
     Log.i("NotificationsService", "Got a remote message 🎉")
     // TODO: Do something useful here
