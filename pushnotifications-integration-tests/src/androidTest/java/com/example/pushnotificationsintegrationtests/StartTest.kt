1779738b63b202bfa10b9d7b2caf30292559bb56
Danielle Vass <danielle.vass@pusher.com>
2019-11-11 14:14:14 +0000

    Enforce singleton to use one instanceId

---
+++ pushnotifications-integration-tests/src/androidTest/java/com/example/pushnotificationsintegrationtests/StartTest.kt
@@ -0,0 +1,124 @@
+package com.example.pushnotificationsintegrationtests
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import com.pusher.pushnotifications.PushNotifications
+import com.pusher.pushnotifications.PushNotificationsInstance
+import com.pusher.pushnotifications.fcm.MessagingService
+import com.pusher.pushnotifications.internal.InstanceDeviceStateStore
+import org.awaitility.core.ConditionTimeoutException
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.untilNotNull
+import org.awaitility.kotlin.untilNull
+import org.awaitility.kotlin.until
+import org.hamcrest.CoreMatchers.*
+import org.junit.After
+import org.junit.AfterClass
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Before
+import java.io.File
+import java.lang.IllegalStateException
+import java.util.concurrent.TimeUnit
+import kotlin.test.asserter
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class StartTest {
+  val context = InstrumentationRegistry.getTargetContext()
+  val instanceId = "00000000-1241-08e9-b379-377c32cd1e80"
+  val instanceId2 = "00000000-1241-08e9-b379-377c32cd1e81"
+  val errolClient = ErrolAPI(instanceId, "http://localhost:8080")
+
+  fun getStoredDeviceId(): String? {
+    val deviceStateStore = InstanceDeviceStateStore(InstrumentationRegistry.getTargetContext(), instanceId)
+    return deviceStateStore.deviceId
+  }
+
+  companion object {
+    val errol = FakeErrol(8080)
+
+    @AfterClass
+    @JvmStatic
+    fun shutdownFakeErrol() {
+      errol.stop()
+    }
+  }
+
+  @Before
+  @After
+  fun wipeLocalState() {
+    val deviceStateStore = InstanceDeviceStateStore(InstrumentationRegistry.getTargetContext(), instanceId)
+
+    await.atMost(1, TimeUnit.SECONDS) until {
+      assertTrue(deviceStateStore.clear())
+
+      deviceStateStore.interests.size == 0 && deviceStateStore.deviceId == null
+    }
+
+    File(context.filesDir, "$instanceId.jobqueue").delete()
+  }
+
+  private fun assertStoredDeviceIdIsNotNull() {
+    try {
+      await.atMost(DEVICE_REGISTRATION_WAIT_SECS, TimeUnit.SECONDS) untilNotNull {
+        getStoredDeviceId()
+      }
+    } catch (e: ConditionTimeoutException) {
+      // Maybe FCM is complaining in CI, so let's pretend to have a token now
+      MessagingService.onRefreshToken!!("fake-fcm-token")
+
+      await.atMost(DEVICE_REGISTRATION_WAIT_SECS, TimeUnit.SECONDS) untilNotNull {
+        getStoredDeviceId()
+      }
+    }
+  }
+
+  @Test
+  fun startShouldRegisterToBeams() {
+    // Start the SDK
+    PushNotifications.start(context, instanceId)
+
+    // A device ID should have been stored
+    assertStoredDeviceIdIsNotNull()
+    val storedDeviceId = getStoredDeviceId()
+
+    // and the stored id should match the server one
+    assertNotNull(errolClient.getDevice(storedDeviceId!!))
+  }
+
+  @Test(expected = IllegalStateException::class)
+  fun startMultipleInstancesWithSingletonThrowsException() {
+    PushNotifications.start(context, instanceId)
+    PushNotifications.start(context, instanceId2)
+  }
+
+  @Test
+  fun startMultipleTimesWithSameInstanceShouldBeIdempotent() {
+    // Start the SDK
+    PushNotifications.start(context, instanceId)
+
+    // A device ID should have been stored
+    assertStoredDeviceIdIsNotNull()
+    val storedDeviceId = getStoredDeviceId()
+
+    // and the stored id should match the server one
+    assertNotNull(errolClient.getDevice(storedDeviceId!!))
+
+    // start the SDK again with the same instanceId
+    PushNotifications.start(context, instanceId)
+
+    assertStoredDeviceIdIsNotNull()
+    val storedDeviceId2 = getStoredDeviceId()
+
+    // check we have the same deviceId
+    assertEquals(storedDeviceId, storedDeviceId2)
+  }
+}
--- pushnotifications/src/main/java/com/pusher/pushnotifications/PushNotifications.java
+++ pushnotifications/src/main/java/com/pusher/pushnotifications/PushNotifications.java
@@ -24,8 +24,14 @@ public class PushNotifications {
      * is deemed better for your project.
      */
     public static PushNotificationsInstance start(Context context, String instanceId) {
-        instance = new PushNotificationsInstance(context, instanceId);
-        instance.start();
+        if (instance == null) {
+            instance = new PushNotificationsInstance(context, instanceId);
+            instance.start();
+        } else if (!instance.getInstanceId().equals(instanceId)) {
+            throw new IllegalStateException("PushNotifications.start has been called before with a different instanceId! (before: "
+                    + instance.getInstanceId() + ", now: " + instanceId + ")");
+        }
+
         return instance;
     }
 
