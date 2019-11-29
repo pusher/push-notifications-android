8ba972a129029523318964bf69ba86eff37c26c0
Luis Fonseca <luis@pusher.com>
2019-11-29 15:29:49 +0000

    Remove unnecessary comment

--- pushnotifications-integration-tests/src/androidTest/java/com/example/pushnotificationsintegrationtests/ReportingTest.kt
+++ pushnotifications-integration-tests/src/androidTest/java/com/example/pushnotificationsintegrationtests/ReportingTest.kt
@@ -22,11 +22,6 @@ import org.junit.runner.RunWith
 import java.io.File
 import java.util.concurrent.TimeUnit
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ReportingTest {
   val context = InstrumentationRegistry.getTargetContext()
