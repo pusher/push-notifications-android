2d8ce5296e612df39a9ba4c3690c67e488173110
Ramon Binz <binzram@gmail.com>
2020-04-15 00:05:37 +0200

    OkHttpClient in reportingAPI as singleton

--- pushnotifications/src/main/java/com/pusher/pushnotifications/reporting/api/ReportingAPI.kt
+++ pushnotifications/src/main/java/com/pusher/pushnotifications/reporting/api/ReportingAPI.kt
@@ -17,11 +17,6 @@ class ReportingAPI(private val instanceId: String) {
 
   private val gson = Gson()
 
-  private val client =
-    OkHttpClient.Builder()
-      .addInterceptor(PusherLibraryHeaderInterceptor())
-      .build()
-
   private val service =
     Retrofit.Builder()
       .baseUrl(baseUrl)
@@ -53,5 +48,13 @@ class ReportingAPI(private val instanceId: String) {
       instanceId = instanceId,
       reportingRequest = reportEvent
     ).enqueue(callback)
+  }
+
+
+  companion object {
+    val client: OkHttpClient =
+      OkHttpClient.Builder()
+        .addInterceptor(PusherLibraryHeaderInterceptor())
+        .build()
   }
 }
